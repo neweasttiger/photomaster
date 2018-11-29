2368f51d7f9028b2ef3b37862cb1918624480d9c
NewEastTiger <generalshin@inu.ac.kr>
2018-11-28 19:04:32 -0800

    recovery sourcefile

---
+++ filesort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <string.h>
+
+int pictur(const struct dirent *info){
+	char *ext;
+	ext = strrchr(info->d_name, '.');
+	if(ext ==NULL){
+		return 0;
+	}
+	if(strcmp(ext, ".jpg") == 0){
+		return 1;
+	}
+	if(strcmp(ext, ".jpeg") == 0){
+		return 1;
+	}else 
+		return 0;
+}
+void facedetect(){
+  struct dirent **namelist;
+  int count;
+  int idx;
+  char file_path[1024];
+  const char *path =".";
+  count  = scandir(path, &namelist, pictur,alphasort);
+  for (idx =0 ; idx< count; idx++){
+	char p[100] = "python faces.py ";
+	strcat(p, namelist[idx]->d_name);
+	printf("%s",p);
+  	system(p);
+
+  }
+  exit(0);
+  
+}
+
+
+int main(){
+	
+		facedetect();
+	
+	
+	
+return 0;
+}
